f3ff4f8bc7be0ddd10efb781d774ec5f9e6a3f65
AivanF <aivanf@mail.ru>
2024-07-08 08:46:44 +0300

    Original W2 v1.3.10

---
+++ control.lua
@@ -0,0 +1,31 @@
+--[[-------------------------------------
+
+Author: Pyro-Fire
+https://mods.factorio.com/mod/warptorio2
+
+Script: control.lua
+Purpose: control stuff
+
+
+Written using Microsoft Notepad.
+IDE's are for children.
+
+How to notepad like a pro:
+ctrl+f = find
+ctrl+h = find & replace
+ctrl+g = show/jump to line (turn off wordwrap n00b)
+
+Status bar wastes screen space, don't use it.
+
+Use https://tools.stefankueng.com/grepWin.html to mass search, find and replace many files in bulk.
+
+]]---------------------------------------
+
+--[[ Environment ]]--
+lib={PLANETORIO=true,REMOTES=true}
+
+require("lib/lib")
+require("control_main")
+lib.lua()
+
+
